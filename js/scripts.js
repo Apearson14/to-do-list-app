b82a97c7826034fea92fd79aa865eb6b8aa10b83
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-05-04 16:40:42 -0400

    Updating first section to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ function newItem(){
 
     //javascript
     //1. Adding a new item to the list of items: 
-    let li = $('li');
+    let li = $('<li>');
     let inputValue = $('#input').val();
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
-    
-       if (inputValue === '') {
-         alert("You must write something!");
-       } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
-       }
+    let text = $("<span>").text(inputValue);
+    li.append(text);
+ 
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      let list = $('#list');
+      list.append(li);
+    }
     
      //2. Crossing out an item from the list of items:
        function crossOut() {
